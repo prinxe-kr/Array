d3cb921b1a78433b7d34a29f248678355ff502ac
prinxe-kr <princekumar5503.real@gmail.com>
2025-11-27 01:43:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let n = [10, 25, 7, 40, 18];// Define an array with at five numerical values
+let s = 0;// Variable to store the sum
+for (let i = 0; i < n.length; i++) // Loop through the array and add each value to the sum
+{
+    s += n[i];
+}
+console.log("The sum : " + s);// Display the final result
+
